5905317e344f63a2cba1d3af3b08a785f0a9cf89
Luis <luisfrb56@gmail.com>
2018-06-11 11:00:27 +0200

    Fin, este es el verdadero programa que tenia hecho

--- src/main/java/org/eclipse/jgit/controlmaven/ControlMaven.java
+++ src/main/java/org/eclipse/jgit/controlmaven/ControlMaven.java
@@ -17,17 +17,43 @@ public class ControlMaven {
       /*
         *Conectamos con la pagina web donde este recogio el tiempo.
         */
-Document doc=Jsoup.connect("https://www.accuweather.com/es/es/spain-weather").get();
+        try {
 
-/*
-*Abrir un vinculo para poder elegir lo que deseamos del documeto
-*/
-       connect(doc.title());
-        Elements newsHeadlines = doc.select("#large-temp");
-        for (Element headline : newsHeadlines) {
-            log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
-        
+            
+
+            // Me conecto a la pagina del tiempo
+
+            Document doc = Jsoup.connect("https://weather.com/es-ES/tiempo/hoy/l/SPXX0084:1:SP").get();
+
+            
+
+            // Mostramos el titulo de dicha pagina
+
+            System.out.println("Titulo de la página: " + doc.title());
+
+            
+
+            // recogemos la temperatura y el tiempo (ambos lo coge por el nombre de la clase, solo selecciona el 1º)
+
+            Element temperatura = doc.select("div.today_nowcard-temp").first();
+
+            Element descTiempo = doc.select("div.today_nowcard-phrase").first();
+
+            
+
+            // Muestro lo anterior recogido
+            System.out.println("La temperatura: " + temperatura.text());
+
+            System.out.println("El tiempo: " + descTiempo.text());
+
+        } catch (IOException ex) {
+
+            
+
+            //Mensaje de error por algun motivo
+
+            System.out.println("La conexion con la pagina ha fallado o no tienes conexion");
+
         }
     }
-    
 }
